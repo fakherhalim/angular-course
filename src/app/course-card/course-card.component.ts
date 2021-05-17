a285ec5044f22ce407c0f7622182d230eef2f64d
Fakher Halim <fakher@tpgxml.com>
2021-05-17 14:00:55 -0500

    ngFor added

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from 'events';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;//it is defined with @input() decorator so that it could be passed on from container with a value
+  @Input()
+  cardIndex: number;
+
+  
+  @Output()
+  courseSelected = new EventEmitter<Course>();//this must be decorated wiht @output() emitter.
+  
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+  }
+  onCourseViewed(){
+    console.log("CourseCardComponet cliked!");
+    this.courseSelected.emit(this.course);
+  }
+}
