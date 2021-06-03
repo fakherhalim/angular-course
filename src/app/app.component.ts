5c57b20453bdb0e49e4ced84d7586e5b293a8a5c
Fakher Halim <fakher@tpgxml.com>
2021-06-03 16:01:04 -0500

    @ViewChild() decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   courses = COURSES;
-  startDate = new Date(2000, 0, 1);
-  title = COURSES[0].description;
-  price = 9.99;
-  rate=.67;
-  course = COURSES[0];
+  // startDate = new Date(2000, 0, 1);
+  // title = COURSES[0].description;
+  // price = 9.99;
+  // rate=.67;
+  // course = COURSES[0];
+
+//@ViewChild is a template querying mechanism. It will return first matching element
+  @ViewChild(CourseCardComponent)
+  card: CourseCardComponent;
 
   onCourseSelected(course: Course){
-    console.log("AppComponent clicked", course)
+    // console.log("AppComponent clicked", course)
+    console.log(this.card);
   }
 }
