ffbf02ce27585c9a6a61b24c2d9a2c3a2a2a1e09
Fakher Halim <fakher@tpgxml.com>
2021-06-03 09:58:44 -0500

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   courses = COURSES;
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.99;
+  rate=.67;
+  course = COURSES[0];
 
   onCourseSelected(course: Course){
     console.log("AppComponent clicked", course)
